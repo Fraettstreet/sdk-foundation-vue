adc77631055b58fec3a1c13540d0746687156959
Fraettstreet <fraettstreet@gmail.com>
2023-04-02 15:21:16 +0200

    change frontend

---
+++ frontend/dist/assets/front-page.2a547f66.js
@@ -0,0 +1,3 @@
+/*! Built using the PremoWeb Software Development Kit for Vue 3 + PHP. Learn more at https://premoweb.com/sdk. */
+  
+import{_ as e,o as n,c as r}from"./index.835444bb.js";const t={};function c(o,s){return n(),r("p",null,"Simple php backend and vue3 frontend. Trying CI here. Hope system is steady!")}const p=e(t,[["render",c]]);export{p as default};

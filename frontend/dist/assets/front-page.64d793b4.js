9cc759933ffa05dfb243c62f51578184a139dbe9
Fraettstreet <fraettstreet@gmail.com>
2023-04-02 14:56:11 +0200

    modify .gitignore

---
+++ frontend/dist/assets/front-page.64d793b4.js
@@ -0,0 +1,3 @@
+/*! Built using the PremoWeb Software Development Kit for Vue 3 + PHP. Learn more at https://premoweb.com/sdk. */
+  
+import{_ as e,o as t,c}from"./index.91c300b6.js";const a={};function n(o,r){return t(),c("p",null,"This is a test page uh yea")}const _=e(a,[["render",n]]);export{_ as default};
